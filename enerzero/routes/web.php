312b72e355e6248aa8436bf782a5078cd97ee83c
sarahlukir <148058280+sarahlukir@users.noreply.github.com>
2025-04-15 17:43:11 +0700

    Delete enerzero directory

--- enerzero/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\LeaderboardController;
-
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/leaderboard', [LeaderboardController::class, 'showUserRank'])->name('leaderboard.index');
