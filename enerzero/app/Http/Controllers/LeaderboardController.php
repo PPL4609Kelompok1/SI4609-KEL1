e3118eddb4471685c67084c261dc2aef4ec0d1eb
sarahlukir <sarahlukiraihani@gmail.com>
2025-04-21 13:16:06 +0700

    SCRUM-18 PBI#12 Sistem Leaderboard

--- enerzero/app/Http/Controllers/LeaderboardController.php
+++ enerzero/app/Http/Controllers/LeaderboardController.php
@@ -9,21 +9,21 @@ class LeaderboardController extends Controller
     public function showUserRank()
     {
         $ranked = [
-            ['rank' => 1, 'name' => 'SHANKSAKAGAMI', 'score' => 12161, 'avatar' => 'https://i.pravatar.cc/150?img=1'],
-            ['rank' => 2, 'name' => 'KEVANNN', 'score' => 12110, 'avatar' => 'https://i.pravatar.cc/150?img=2'],
-            ['rank' => 3, 'name' => 'MANCCA', 'score' => 12104, 'avatar' => 'https://i.pravatar.cc/150?img=3'],
-            ['rank' => 4, 'name' => 'CHUBA', 'score' => 11855, 'avatar' => 'https://i.pravatar.cc/150?img=4'],
-            ['rank' => 5, 'name' => 'NANDI', 'score' => 11780, 'avatar' => 'https://i.pravatar.cc/150?img=5'],
-            ['rank' => 6, 'name' => 'LUKA', 'score' => 11720, 'avatar' => 'https://i.pravatar.cc/150?img=6'],
-            ['rank' => 7, 'name' => 'FARHAN', 'score' => 11688, 'avatar' => 'https://i.pravatar.cc/150?img=7'],
-            ['rank' => 8, 'name' => 'SALMA', 'score' => 11650, 'avatar' => 'https://i.pravatar.cc/150?img=8'],
-            ['rank' => 9, 'name' => 'ARIEL', 'score' => 11580, 'avatar' => 'https://i.pravatar.cc/150?img=9'],
-            ['rank' => 10, 'name' => 'FIRMAN', 'score' => 11560, 'avatar' => 'https://i.pravatar.cc/150?img=10'],
-            ['rank' => 11, 'name' => 'RAISYA', 'score' => 11490, 'avatar' => 'https://i.pravatar.cc/150?img=11'],
-            ['rank' => 12, 'name' => 'BINTANG', 'score' => 11400, 'avatar' => 'https://i.pravatar.cc/150?img=12'],
-            ['rank' => 13, 'name' => 'ALYA', 'score' => 11350, 'avatar' => 'https://i.pravatar.cc/150?img=13'],
-            ['rank' => 14, 'name' => 'NABIL', 'score' => 11300, 'avatar' => 'https://i.pravatar.cc/150?img=14'],
-            ['rank' => 15, 'name' => '[username]', 'score' => 11250, 'avatar' => 'https://i.pravatar.cc/150?img=15'],
+            ['rank' => 1, 'name' => 'Aisyah', 'score' => 3000, 'avatar' => 'https://i.pravatar.cc/150?img=1'],
+            ['rank' => 2, 'name' => 'Raka', 'score' => 2800, 'avatar' => 'https://i.pravatar.cc/150?img=2'],
+            ['rank' => 3, 'name' => 'Zidan', 'score' => 2500, 'avatar' => 'https://i.pravatar.cc/150?img=3'],
+            ['rank' => 4, 'name' => 'Nisa', 'score' => 2400, 'avatar' => 'https://i.pravatar.cc/150?img=4'],
+            ['rank' => 5, 'name' => 'Bima', 'score' => 2300, 'avatar' => 'https://i.pravatar.cc/150?img=5'],
+            ['rank' => 6, 'name' => 'Fahri', 'score' => 2200, 'avatar' => 'https://i.pravatar.cc/150?img=6'],
+            ['rank' => 7, 'name' => 'Ari', 'score' => 2100, 'avatar' => 'https://i.pravatar.cc/150?img=7'],
+            ['rank' => 8, 'name' => 'Tari', 'score' => 2000, 'avatar' => 'https://i.pravatar.cc/150?img=8'],
+            ['rank' => 9, 'name' => 'Maya', 'score' => 1900, 'avatar' => 'https://i.pravatar.cc/150?img=9'],
+            ['rank' => 10, 'name' => 'Yuli', 'score' => 1800, 'avatar' => 'https://i.pravatar.cc/150?img=10'],
+            ['rank' => 11, 'name' => 'Rizky', 'score' => 1700, 'avatar' => 'https://i.pravatar.cc/150?img=11'],
+            ['rank' => 12, 'name' => 'Chandra', 'score' => 1600, 'avatar' => 'https://i.pravatar.cc/150?img=12'],
+            ['rank' => 13, 'name' => 'Mila', 'score' => 1500, 'avatar' => 'https://i.pravatar.cc/150?img=13'],
+            ['rank' => 14, 'name' => 'Miko', 'score' => 1400, 'avatar' => 'https://i.pravatar.cc/150?img=14'],
+            ['rank' => 15, 'name' => '[username]', 'score' => 1300, 'avatar' => 'https://i.pravatar.cc/150?img=15'],
         ];
 
         $userRank = collect($ranked)->firstWhere('name', '[username]');
